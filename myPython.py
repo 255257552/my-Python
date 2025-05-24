b5d3424aa02d2f864374e5c575b669f84e3d6262
255257552 <mcpppsse@qq.com>
2025-05-24 12:42:32 +0800

    Update and rename README.md to myPython.py
    create a py file

---
+++ myPython.py
@@ -0,0 +1,10 @@
+# my-Python
+print('Is it my Python')
+#打印Is it my python
+
+#四舍五入计算器
+number = float(input('请输入数字:'))
+if number - int(number) ＞4:
+  print(int(number) + 1)  #如果小数部分大于4，则去小数加1
+else:
+  print(int(number))     #如果小数部分不大于4，则去小数
